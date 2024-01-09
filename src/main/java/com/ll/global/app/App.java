a01af32a0894106019acd6589eae93379c21e380
naraekwon86 <wde0530@gmail.com>
2024-01-09 20:51:24 +0900

    lesson 5

---
+++ src/main/java/com/ll/global/app/App.java
@@ -0,0 +1,18 @@
+package com.ll.global.app;
+
+import java.util.Scanner;
+
+public class App {
+    private final Scanner scanner;
+
+    public App(final Scanner scanner){
+        this.scanner = scanner;
+    }
+    public void run(){
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명언 : ");
+        System.out.print("작가 : ");
+
+    }
+}
---
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -0,0 +1,54 @@
+package com.ll.global.app;
+
+import com.ll.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    private String run(final String cmd){
+        final Scanner scanner = TestUtil.genScanner(cmd.stripIndent().trim());
+
+        final ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        new App(scanner).run();
+
+        final String out = byteArrayOutputStream.toString().trim();
+        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+        return out.trim();
+    }
+    @Test
+    @DisplayName("프로그램 시작 시 \"== 명언앱 ==\" 출력")
+    void t1(){
+        final String out = run("""
+                종료""");
+        assertThat(out)
+                .contains("== 명언 앱 ==");
+    }
+    @Test
+    @DisplayName("종료")
+    void t2(){
+        final String out = run("""
+                종료
+                """);
+    }
+    @Test
+    @DisplayName("등록")
+    void t3(){
+        final String out = run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                종료
+                """);
+
+        assertThat(out)
+                .contains("명언 :")
+                .contains("작가 :");
+    }
+}
