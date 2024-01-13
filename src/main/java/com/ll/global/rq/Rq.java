7de7b770a6d4465e831e41b544c28608d6be7319
naraekwon86 <wde0530@gmail.com>
2024-01-13 10:30:12 +0900

    lesson 10

---
+++ src/main/java/com/ll/global/rq/Rq.java
@@ -0,0 +1,34 @@
+package com.ll.global.rq;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Rq {
+    private final String action;
+    private final String queryString;
+    private Map<String, String> params;
+
+
+    public Rq(final String cmd){
+        final String[] cmdBits = cmd.split("\\?",2);
+        action = cmdBits[0].trim();
+        queryString = cmdBits.length == 2 ? cmdBits[1].trim() : "";
+        params = new HashMap<>();
+
+        if (queryString.isBlank()) return;
+
+        final String[] paramBits = queryString.split("=", 2);
+        final String paramName = paramBits[0].trim();
+        final String paramValue = paramBits[1].trim();
+
+        params.put(paramName, paramValue);
+    }
+    public String getAction() {
+        return action;
+    }
+    public String getParameter(final String paramName){
+        return params.get(paramName);
+    }
+
+
+}
