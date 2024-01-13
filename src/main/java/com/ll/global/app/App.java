d0f851d1ad81751d3a89c3d677214bdc240cf420
naraekwon86 <wde0530@gmail.com>
2024-01-13 13:05:25 +0900

    lesson 14

--- src/main/java/com/ll/global/app/App.java
+++ src/main/java/com/ll/global/app/App.java
@@ -29,27 +29,37 @@ public class App {
             case "삭제" ->{
                 final long id = rq.getParameterAsLong("id",0);
                 quotations
-                        .removeIf(quotation -> quotation.getId() == id);
-                System.out.println("%d번 명언이 삭제되었습니다.");
+                        .stream()
+                        .filter(quotation -> quotation.getId() == id)
+                        .findFirst()
+                        .ifPresentOrElse(
+                                quotation -> {
+                                    quotations.remove(quotation);
+                                    System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
+                                },
+                                () ->System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id))
+                        );
             }
             case "수정" ->{
                 final long id = rq.getParameterAsLong("id",0);
-                final Quotation quotation = quotations
+                quotations
                         .stream()
                         .filter(_quotation -> _quotation.getId() == id)
                         .findFirst()
-                        .get();
-                System.out.println("명언(기존) : %s".formatted(quotation.getContent()));
-                System.out.print("명언 : ");
-                final String content = scanner.nextLine().trim();
-                System.out.println("작가(기존) : %s".formatted(quotation.getAuthorName()));
-                System.out.print("작가 : ");
-                final String authorName = scanner.nextLine().trim();
-
-                quotation.setContent(content);
-                quotation.setAuthorName(authorName);
-
-                System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
+                        .ifPresentOrElse(
+                            quotation -> {
+                                System.out.println("명언(기존) : %s".formatted(quotation.getContent()));
+                                System.out.print("명언 : ");
+                                final String content = scanner.nextLine().trim();
+                                System.out.println("작가(기존) : %s".formatted(quotation.getAuthorName()));
+                                System.out.print("작가 : ");
+                                final String authorName = scanner.nextLine().trim();
+                            quotation.setContent(content);
+                            quotation.setAuthorName(authorName);
+                            System.out.println("%d번 명언이 수정되었습니다.".formatted(id));
+                            },
+                () -> System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id))
+                                );
             }
             case "등록" -> {
                 System.out.print("명언 : ");
--- src/test/java/com/ll/global/app/AppTest.java
+++ src/test/java/com/ll/global/app/AppTest.java
@@ -176,4 +176,42 @@ public class AppTest {
                 .contains("2 / 홍길동님 / 과거에 집착하지 마라!")
                 .contains("1 / 작자미상 / 현재를 사랑하라.");
     }
+    @Test
+    @DisplayName("존재하지 않는 명언에 대한 삭제 예외처리")
+    void t11(){
+        final String out = run("""
+          등록
+          현재를 사랑하라.
+          작자미상
+          등록
+          과거에 집착하지 마라.
+          홍길동
+          삭제?id=2
+          삭제?id=3
+          """);
+        assertThat(out)
+                .contains("2번 명언이 삭제되었습니다.")
+                .contains("3번 명언은 존재하지 않습니다.");
+
+    }
+    @Test
+    @DisplayName("존재하지 않는 명언에 대한 수정 예외처리")
+    void t12(){
+        final String out = run("""
+          등록
+          현재를 사랑하라.
+          작자미상
+          등록
+          과거에 집착하지 마라.
+          홍길동
+          수정?id=2
+          과거에 집착하지 마라!
+          홍길동님
+          수정?id=3
+          """);
+        assertThat(out)
+                .contains("2번 명언이 수정되었습니다.")
+                .contains("3번 명언은 존재하지 않습니다.");
+
+    }
 }
